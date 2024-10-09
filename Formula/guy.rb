71107231011c5b08f6c9b96e755ce4aff0f9a7c4
asafshpigler <asafshpigler@gmail.com>
2024-10-09 12:43:37 +0100

    chore: update formula

--- Formula/guy.rb
+++ Formula/guy.rb
@@ -1,7 +1,7 @@
 class Guy < Formula
     desc "A package that explains guy"
     homepage "https://github.com/asafshpigler/guy"
-    url "https://github.com/asafshpigler/guy/archive/v1.0.0.tar.gz"
+    url "https://github.com/asafshpigler/guy/blob/main/v1.0.0.tar.gz"
     sha256 "5416903924a4308d63ac269bca43d477696e5bf6dac8d9a5aae8fb6fb5eb30fc"
     version "1.0.0"
   
