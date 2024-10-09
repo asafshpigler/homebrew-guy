0df48f3f60c56229b766656a0b042e940981bbaa
asafshpigler <asafshpigler@gmail.com>
2024-10-09 13:01:27 +0100

    chore: update release hash

--- Formula/guy.rb
+++ Formula/guy.rb
@@ -2,7 +2,7 @@ class Guy < Formula
     desc "A package that explains guy"
     homepage "https://github.com/asafshpigler/guy"
     url "https://github.com/asafshpigler/guy/archive/refs/tags/v1.0.0.tar.gz"
-    sha256 "1e170adb2f8ab82faddc27267b54a8425c27b1fa9ab76a7cc77d654d2d55154b"
+    sha256 "0313f29043fbf39fd8acd534ff6de41a508fab9d0742c3552a5b7c18326e5ac6"
     version "1.0.0"
   
     def install
