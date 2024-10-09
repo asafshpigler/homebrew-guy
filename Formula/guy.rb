c6a930b8ea4ce1cfc67de26b3cb924a20fcd62c7
asafshpigler <asafshpigler@gmail.com>
2024-10-09 12:51:14 +0100

    chore: update formula

--- Formula/guy.rb
+++ Formula/guy.rb
@@ -1,8 +1,8 @@
 class Guy < Formula
     desc "A package that explains guy"
     homepage "https://github.com/asafshpigler/guy"
-    url "https://github.com/asafshpigler/guy/blob/main/v1.0.0.tar.gz"
-    sha256 "a7c0601960de8bbec204c13f6adb0e1926e4c8dc9a00198ddd7d991423849ce7"
+    url "https://github.com/asafshpigler/guy/archive/refs/tags/v1.0.0.tar.gz"
+    sha256 "0313f29043fbf39fd8acd534ff6de41a508fab9d0742c3552a5b7c18326e5ac6"
     version "1.0.0"
   
     def install
